f0f012ab154d7b211e6a849c63733fe9fe2cd889
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-06-18 14:18:39 +0200

    Moved wsgi.py to the root folder.

--- wsgi.py
+++ wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
 load_dotenv()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
 application = get_wsgi_application()
