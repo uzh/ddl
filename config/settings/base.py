0a8b37edd6d205c530775edeb1c30eee73ee6a30
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-06-23 13:22:37 +0200

    Adjust BASE_DIR path.

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = ['.datadonation.uzh.ch', '.data-donation.uzh.ch', 'localhost']
 
