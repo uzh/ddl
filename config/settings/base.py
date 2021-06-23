9d2cef70605fc54852daaef5c6ad5eda2a73f92f
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-06-23 12:08:06 +0200

    Updated Allowed Hosts.

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.datadonation.uzh.ch', '.data-donation.uzh.ch', 'localhost']
 
 # APPLICATION DEFINITIONS
 # ------------------------------------------------------------------------------
