88e71af2dec0e682a926c8b43cbdf4540c03ac9b
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-07-13 18:34:49 +0200

    Change admin slug.

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('control/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
