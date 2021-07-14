b240485a1a4a4b60d967a57de1007dc72f985675
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-07-14 15:45:24 +0200

    Added javascript for Matomo Tracking.

--- mainpage/static/mainpage/js/base.js
+++ mainpage/static/mainpage/js/base.js
@@ -22,3 +22,22 @@ $(document).ready(function() {
         $(".modal").removeClass("is-active")
     });
 });
+
+
+// Matomo
+var _paq = _paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['setAPIUrl', "https://webstats.uzh.ch/"]); // overlay fix
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//webstats.uzh.ch/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '407']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript';
+    g.async=true;
+    g.defer=true;
+    g.src=u+'piwik.js';
+    s.parentNode.insertBefore(g,s);
+})();
