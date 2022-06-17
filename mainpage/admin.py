81cfb8e265cbe49bf1ecd7ffbbecab8fb5f5ff75
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-06-17 15:26:06 +0200

    feat: Added Wagtail cms.

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-
-from .admin_actions import export_as_csv, export_forms_as_csv
-
-
-admin.site.add_action(export_as_csv, 'export_selected')
-admin.site.add_action(export_forms_as_csv, 'export_selected_forms')
