86716234262362e780624f673ea8e5521c409bd3
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2023-08-29 17:43:11 +0200

    Delete PDF view in URLs.

--- digital_meal/urls.py
+++ digital_meal/urls.py
@@ -6,6 +6,5 @@ from digital_meal import views as dm_views
 urlpatterns = [
     path('report/youtube/<slug:external_participant_id>', dm_views.IndividualReport.as_view(), name='individual-report'),
     path('report/fitbit/<slug:external_participant_id>', dm_views.IndividualFitbitReport.as_view(), name='individual-fitbit-report'),
-    path('report/pdf/<slug:external_pool_project_id>/<slug:external_pool_participant_id>', dm_views.IndividualReportPDF.as_view(), name='individual-report-as-pdf'),
     path('', dm_views.ScientificaLandingView.as_view(), name='scientifica')
 ]
