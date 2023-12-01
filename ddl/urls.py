4f1412a40ecabb7a744809a265c495332c27cf32
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2023-12-01 09:39:16 +0100

    Add landing pages for VP study.

--- ddl/urls.py
+++ ddl/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from ddl.views import VPStudyLandingPage, VPStudyLandingPageInvited
 
 
 urlpatterns = [
+    path('vp-studie', VPStudyLandingPage.as_view(), name='vp-study'),
+    path('vp-studie-inv', VPStudyLandingPageInvited.as_view(), name='vp-study-invited')
 ]
--- ddl/views.py
+++ ddl/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class MainView(TemplateView):
     template_name = 'ddl/base.html'
+
+
+class VPStudyLandingPage(TemplateView):
+    template_name = 'ddl/vp_study_landing.html'
+
+
+class VPStudyLandingPageInvited(TemplateView):
+    template_name = 'ddl/vp_study_landing_invited.html'
