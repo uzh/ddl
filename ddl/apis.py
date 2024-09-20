5e6c9f5090fc17cd7f3e0fd15a2960e5797028fc
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2024-09-20 16:46:50 +0200

    feat: Add alternative response api endpoint.

--- ddl/apis.py
+++ ddl/apis.py
@@ -19,7 +19,7 @@ from ddm.models.serializers import (
 )
 from ddm.views.apis import DDMAPIMixin, user_is_allowed
 
-from rest_framework import authentication, permissions, serializers
+from rest_framework import authentication, permissions, serializers, status
 from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -271,3 +271,49 @@ class IndividualReportAPI(DDMBaseProjectApi):
             raise PermissionDenied
 
         return True
+
+
+class ResponsesAPIAlt(APIView, DDMAPIMixin):
+    """
+    Retrieve a set of Questionnaire Responses collected for a given Donation Project.
+    """
+    authentication_classes = [ProjectTokenAuthenticator]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_responses(self, project):
+        participants = self.request.query_params.get('participants')
+        if participants is not None:
+            return QuestionnaireResponse.objects.filter(
+                project=project, participant__external_id__in=participants.split(','))
+        else:
+            return QuestionnaireResponse.objects.filter(project=project)
+
+    @sensitive_variables()
+    def get(self, request, format=None, *args, **kwargs):
+        project = self.get_project()
+
+        if project.super_secret:
+            msg = 'Endpoint disabled for super secret projects.'
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                            data={'message': msg})
+
+        try:
+            decryptor = Decryption(project.secret_key, project.get_salt())
+            responses = [ResponseSerializer(r, decryptor=decryptor).data
+                         for r in self.get_responses(project)]
+
+        except ValueError:
+            self.create_event_log(
+                descr='Failed Attempt',
+                msg='Download requested with incorrect secret.'
+            )
+            msg = 'Incorrect key material.'
+            return Response(status=status.HTTP_403_FORBIDDEN,
+                            data={'message': msg})
+
+        response = Response(responses)
+        self.create_event_log(
+            descr='Data Download Successful',
+            msg='The project data was downloaded.'
+        )
+        return response
--- ddl/urls.py
+++ ddl/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from ddl.views import ProjectDataAPIAlt
+from ddl.views import ProjectDataAPIAlt, ResponsesAPIAlt
 
 
 urlpatterns = [
     path('project/<int:pk>/data-alt', ProjectDataAPIAlt.as_view(), name='ddm-data-api'),
+    path('project/<int:pk>/responses-alt', ResponsesAPIAlt.as_view(), name='alt-response-api'),
 ]
