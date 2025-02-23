de2c29ead3c3e563a6689ce2ee004037cdb35710
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-23 13:43:25 +0100

    refactor: Remove APIs and views that are now integrated in DDM v2.x.

--- ddl/urls.py
+++ ddl/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
-from ddl.views import ProjectDataAPIAlt
-from ddl.apis import ResponsesAPIAlt
 
 
-urlpatterns = [
-    path('project/<int:pk>/data-alt', ProjectDataAPIAlt.as_view(), name='ddm-data-api'),
-    path('project/<int:pk>/responses-alt', ResponsesAPIAlt.as_view(), name='alt-response-api'),
-]
+urlpatterns = []
