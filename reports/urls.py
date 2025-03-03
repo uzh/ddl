32cc961b413ffd9f76f76c23870a29c6abca269a
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-03 11:38:26 +0100

    chore: Update ChatGPT report to work with updated DDM API.

--- reports/urls.py
+++ reports/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import (
-    PoliticsReportInstagram, PoliticsReportFacebook, SearchReport,
-    DigitalMealReport, ChatGPTReport
-)
+from .views import SearchReport, DigitalMealReport, ChatGPTReport
 
 
 urlpatterns = [
-    path('politik-instagram/<slug:participant_id>', PoliticsReportInstagram.as_view(), name='politics-report-instagram'),
-    path('politik-facebook/<slug:participant_id>', PoliticsReportFacebook.as_view(), name='politics-report-facebook'),
     path('google/<slug:participant_id>', SearchReport.as_view(), name='search-report'),
     path('digital-meal/<slug:participant_id>', DigitalMealReport.as_view(), name='digital-meal-report'),
     path('chat-gpt/<slug:participant_id>', ChatGPTReport.as_view(), name='chatgpt-report'),
