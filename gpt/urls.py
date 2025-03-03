4acf12abb970101c31429db14efa29c3a121f544
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-03 11:53:20 +0100

    chore: Update ChatGPT report url to a more meaningful pattern.

--- gpt/urls.py
+++ gpt/urls.py
@@ -6,5 +6,5 @@ from .views import BriefingViewGPT, DataDonationViewGPT
 urlpatterns = [
     path('datadonation/<slug:slug>/briefing/', BriefingViewGPT.as_view(), name='briefing-gpt'),
     path('datadonation/<slug:slug>/data-donation/', DataDonationViewGPT.as_view(), name='data-donation-gpt'),
-    path('datadonation/report/<slug:participant_id>/', ChatGPTReport.as_view(), name='gpt-report'),
+    path('datadonation/chatgpt-report/<slug:participant_id>/', ChatGPTReport.as_view(), name='gpt-report'),
 ]
